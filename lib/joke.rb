534b6ac26b2e60edc066ba4fcc8efea0342bcac8
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-13 09:08:31 -0700

    Joke class started

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,9 @@
 class Joke
 
+  def initialize(joke_data)
+    @id       = joke_data[:id]
+    @question = joke_data[:question]
+    @answer   = joke_data[:answer]
+  end
+
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,12 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def test_that_class_exists
+    joke = Joke.new({id: 1,
+                    question: "Why did the strawberry cross the road?",
+                    answer: "Because his mother was in a jam."})
+
+    assert_instance_of Joke, joke
+  end
+
 end
