fa240adebd28f875406ceb5308119202a0f1d275
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-13 09:11:04 -0700

    more tests passing

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,5 +1,5 @@
 class Joke
-
+  attr_reader :id, :question, :answer
   def initialize(joke_data)
     @id       = joke_data[:id]
     @question = joke_data[:question]
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -13,4 +13,14 @@ class JokeTest < Minitest::Test
     assert_instance_of Joke, joke
   end
 
+  def test_default_attributes
+    joke = Joke.new({id: 2,
+                    question: "How do you keep a lion from charging?",
+                    answer: "Take away its credit cards."})
+
+    assert_equal 2, joke.id
+    assert_equal "How do you keep a lion from charging?", joke.question
+    assert_equal "Take away its credit cards.", joke.answer
+  end
+
 end
